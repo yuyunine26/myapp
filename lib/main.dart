9ec3ce0303c8f0556362ef621f413bf01b9445aa
yuyunine26 <117963966+yuyunine26@users.noreply.github.com>
2023-06-08 23:17:24 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginPage(),
+    ); // MaterialApp
+  }
+}
+ class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Myapp"),
+      ),
+    );
+  }
+}
